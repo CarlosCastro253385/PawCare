feb2f4d69e2d6f7dbe9bfd9d11d81955388e83a0
CarlosCastro253385 <253385@it2id.upchiapas.edu.mx>
2026-07-20 21:37:35 -0600

    Fix: actualizar backend URL a pawcare-api-2026

--- js/config.js
+++ js/config.js
@@ -1,2 +1,12 @@
-// js/config.js
-const API_BASE = 'https://pawcare.loca.lt/api';
+const API_BASE = 'https://pawcare-api-2026.loca.lt/api';
+
+const originalFetch = window.fetch;
+window.fetch = async function(...args) {
+    let [resource, config] = args;
+    config = config || {};
+    config.headers = {
+        ...config.headers,
+        'Bypass-Tunnel-Remainder': 'true'
+    };
+    return originalFetch(resource, config);
+};
